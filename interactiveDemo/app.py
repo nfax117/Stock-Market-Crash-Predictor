36971220ba66e95f511ead828d78d4e538229c34
Cesar Alonso Guzman Avina <cguzmanavina2001@gmail.com>
2022-05-20 00:45:22 -0700

    Created bulk of html and app file
    TODO: add css and connect the backend

--- interactiveDemo/app.py
+++ interactiveDemo/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-from wtforms import Form, BooleanField, StringField, validators
+from wtforms import Form, validators, StringField
 import os
 import sys
 import flask_backend
@@ -8,35 +8,34 @@ app = Flask(__name__)
 app.debug=True
 app.secret_key = "super secret key"
 
-@app.route('/interactiveDemo')
+#P/E Ratio, Open, Low, High, Volume, and Closing Price
+
+class InputForm(Form):
+    day1 = StringField('Day 1', validators=[validators.InputRequired()])
+    day2 = StringField('Day 2', validators=[validators.InputRequired()])
+    day3 = StringField('Day 3', validators=[validators.InputRequired()])
+    day4 = StringField('Day 4', validators=[validators.InputRequired()])
+    day5 = StringField('Day 5', validators=[validators.InputRequired()])
+    day6 = StringField('Day 6', validators=[validators.InputRequired()])
+    day7 = StringField('Day 7', validators=[validators.InputRequired()])
+    day8 = StringField('Day 8', validators=[validators.InputRequired()])
+    day9 = StringField('Day 9', validators=[validators.InputRequired()])
+    day10 = StringField('Day 10', validators=[validators.InputRequired()])
+    day11 = StringField('Day 11', validators=[validators.InputRequired()])
+    day12 = StringField('Day 12', validators=[validators.InputRequired()])
+    day13 = StringField('Day 13', validators=[validators.InputRequired()])
+    day14 = StringField('Day 14', validators=[validators.InputRequired()])
+
+
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-    return render_template('home.html')
-
-@app.route('/home')
-def home():
-    return render_template('home.html')
-
-
-class RegisterForm(Form):
-    name = StringField('', [validators.Length(min=1, max=500)], render_kw={"placeholder": "Please Enter a statement here"})
-
-
-
-# connect and run the python backend
-@app.route('/code', methods=['GET', 'POST'])
-def register():
-    form = RegisterForm(request.form)
-    if request.method == 'POST' and form.validate():
-        name = form.name.data
-        
-
-        # call the run function from the imported python file
-        out = imdb_classifer.run(name)
-        output=str(out)
-
-        return render_template("outputs/"+output+'.html')
-    return render_template('code.html', form=form)
-
+    form = InputForm()
+    # if request.method == 'POST' and form.validate():
+        #parse form info and call backend
+        # out = flask_backend.run(name)
+        # output=str(out)
+        # return render_template("outputs/"+output+'.html')
+    return render_template('home.html', form = form)
 
 if __name__=='__main__':
     app.run()
